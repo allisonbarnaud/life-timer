8966902c31a0544037f729c66984d80ecb14900a
Allison Arnaud <arnaudallisonb@gmail.com>
2021-06-10 23:54:22 +1000

    added timer feature to Dom

---
+++ timer.js
@@ -0,0 +1,31 @@
+var counter_list = [0,0,0];
+  var str_counter_0 = counter_list[0];
+  var str_counter_1 = counter_list[1];
+  var str_counter_2 = counter_list[2];
+  var display_str = "";
+  var display_div = document.getElementById("display_div_id");
+
+  function incrementCount(current_count){
+    setInterval(function(){
+      // clear count
+      while (display_div.hasChildNodes()) {
+          display_div.removeChild(display_div.lastChild);
+      }
+      str_counter_0++;
+      if (str_counter_0 > 59) {
+        str_counter_0 = 0; // reset count
+        str_counter_1++;   // increase next count
+      }
+      if (str_counter_1 > 59) {
+        str_counter_1 = 0;
+        str_counter_2++;
+      }
+      display_str = str_counter_2.toString() + " hour(s), " + str_counter_1.toString() + " minute(s), " + str_counter_0.toString() + " second(s).";
+      for (var i = 0; i < display_str.length; i++) {
+        var new_span = document.createElement('span');
+        new_span.className = 'num_tiles';
+        new_span.innerText = display_str[i];
+        display_div.appendChild(new_span);
+      }
+    },1000);
+  }
