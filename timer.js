0055a22a898077194b102c9c236068536305a6f7
Allison Arnaud <arnaudallisonb@gmail.com>
2021-06-13 16:42:44 +1000

    first commit. main timer features done

--- timer.js
+++ timer.js
@@ -1,26 +1,51 @@
-var counter_list = [0,0,0];
-  var str_counter_0 = counter_list[0];
-  var str_counter_1 = counter_list[1];
-  var str_counter_2 = counter_list[2];
-  var display_str = "";
-  var display_div = document.getElementById("display_div_id");
+var counter_list = [0,0,0,0];
+var secondsCounter = counter_list[0];
+var minutesCounter = counter_list[1];
+var hoursCounter = counter_list[2];
+var daysCounter = counter_list[3];
+var display_str = "";
+var display_div = document.getElementById("display_div_id");
 
-  function incrementCount(current_count){
+var lifeExpectancy = 84;
+var checkDate = new Date(); 
+var currentDate = "Time left: " + checkDate.getDate() + "/"
+                + (checkDate.getMonth()+1)  + "/" 
+                + checkDate.getFullYear();
+
+var currentTime = checkDate.getHours() + ":"  
+                + checkDate.getMinutes() + ":" 
+                + checkDate.getSeconds();
+                
+
+  function incrementCount(){
+
+    var ageInput = parseInt(document.getElementById("age").value);
+    secondsCounter += ageInput;
+
+    var finalDate = "End date: " + checkDate.getDate() + "/"
+                + (checkDate.getMonth()+1)  + "/" 
+                + (checkDate.getFullYear()+lifeExpectancy-ageInput)
+
+    console.log(finalDate)
+
     setInterval(function(){
+      console.log(currentDate)
+      console.log(currentTime)
       // clear count
       while (display_div.hasChildNodes()) {
           display_div.removeChild(display_div.lastChild);
       }
-      str_counter_0++;
-      if (str_counter_0 > 59) {
-        str_counter_0 = 0; // reset count
-        str_counter_1++;   // increase next count
+      
+      secondsCounter++;
+      if (secondsCounter > 59) {
+        secondsCounter = 0; // reset count
+        minutesCounter++;   // increase next count
       }
-      if (str_counter_1 > 59) {
-        str_counter_1 = 0;
-        str_counter_2++;
+      if (minutesCounter > 59) {
+        minutesCounter = 0;
+        hoursCounter++;
       }
-      display_str = str_counter_2.toString() + " hour(s), " + str_counter_1.toString() + " minute(s), " + str_counter_0.toString() + " second(s).";
+      display_str = hoursCounter.toString() + " hour(s), " + minutesCounter.toString() + " minute(s), " + secondsCounter.toString() + " second(s).";
       for (var i = 0; i < display_str.length; i++) {
         var new_span = document.createElement('span');
         new_span.className = 'num_tiles';
